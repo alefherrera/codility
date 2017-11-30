79883569b72f48e9f005d105a95905022d09bcef
Alejandro Herrera <aherrera@hasar.com>
2017-11-30 14:11:02 -0300

    iterations

---
+++ lessons/iterations/index.js
@@ -0,0 +1,14 @@
+function solution(N) {
+    // write your code in JavaScript (Node.js 6.4.0)
+    const binary = N.toString(2);
+    let aux = 0, gap = 0;
+    for(let i = 0; i < binary.length; i++) {
+        if (binary[i] === "1") {
+            gap = Math.max(gap, aux);
+            aux = 0;
+        } else {
+            aux += 1;
+        }
+    }
+    return gap;
+}
